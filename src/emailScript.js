e1c14673da2fd7b73aaff2ca73a33bac015ecee3
Jasoonii <jason.ddang1@gmail.com>
2024-06-12 15:45:06 -0500

    Created sendEmail function

--- src/emailScript.js
+++ src/emailScript.js
@@ -1,3 +1,10 @@
 function SendMail() {
-    
+    var parameters = {
+        from_name : document.getElementById("fullName.value"),
+        email_id : document.getElementById("emailID").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_qbigr5r", "template_s0zvq3q", params).then(function (res) {
+        alert("Email Sent!");
+    })
 }
